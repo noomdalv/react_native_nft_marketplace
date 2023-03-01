b595df618dad29c404550c4e49eaf8067da4c266
noomdalv <vladlunasan@gmail.com>
2023-03-01 09:05:34 -0500

    imported assets and constants, created StatusBar component

--- App.js
+++ App.js
@@ -5,6 +5,12 @@ import { useFonts } from "expo-font";
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 
+import InterBold from "./assets/fonts/Inter-Bold.ttf";
+import InterSemiBold from "./assets/fonts/Inter-SemiBold.ttf";
+import InterMedium from "./assets/fonts/Inter-Medium.ttf";
+import InterRegular from "./assets/fonts/Inter-Regular.ttf";
+import InterLight from "./assets/fonts/Inter-Light.ttf";
+
 const Stack = createStackNavigator();
 
 const theme = {
@@ -16,6 +22,16 @@ const theme = {
 };
 
 const App = () => {
+  const [loaded] = useFonts({
+    InterBold,
+    InterSemiBold,
+    InterMedium,
+    InterRegular,
+    InterLight,
+  });
+
+  if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
