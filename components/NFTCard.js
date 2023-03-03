3e0eca0122b3c5bdf43c420ef8c72d8c7306e126
noomdalv <vladlunasan@gmail.com>
2023-03-03 00:27:46 -0500

    implemented NFTCARD SubInfo details

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -34,6 +34,37 @@ const NFTCard = ({ data }) => {
       </View>
 
       <SubInfo />
+
+      <View
+        style={{
+          width: "100%",
+          padding: SIZES.font,
+        }}
+      >
+        <NFTTitle
+          title={data.name}
+          subtitle={data.creator}
+          titleSize={SIZES.large}
+          subtitleSize={SIZES.small}
+        />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,18 +1,36 @@
 import { View, Text, Image } from "react-native";
 import { SIZES, FONTS, COLORS, SHADOWS, assets } from "../constants";
 
-export const NFTTitle = () => {
+export const NFTTitle = ({ title, subtitle, titleSize, subtitleSize }) => {
   return (
     <View>
-      <Text>NFTTitle</Text>
+      <Text style={{ fontFamily: FONTS.semiBold, fontSize: titleSize }}>
+        {title}
+      </Text>
+      <Text style={{ fontFamily: FONTS.regular, fontSize: subtitleSize }}>
+        {subtitle}
+      </Text>
     </View>
   );
 };
 
-export const EthPrice = () => {
+export const EthPrice = ({ price }) => {
   return (
-    <View>
-      <Text>EthPrice</Text>
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <Image
+        source={assets.eth}
+        resizeMode={"contain"}
+        style={{ width: 20, height: 20, marginRight: 2 }}
+      />
+      <Text
+        style={{
+          fontFamily: FONTS.medium,
+          fontSize: SIZES.font,
+          color: COLORS.primary,
+        }}
+      >
+        {price}
+      </Text>
     </View>
   );
 };
@@ -49,8 +67,36 @@ export const People = () => {
 
 export const EndDate = () => {
   return (
-    <View>
-      <Text>EndDate</Text>
+    <View
+      style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent: "center",
+        alignItems: "center",
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: "50%",
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.primary,
+        }}
+      >
+        Ending in
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.medium,
+          color: COLORS.primary,
+        }}
+      >
+        12h 30m
+      </Text>
     </View>
   );
 };
